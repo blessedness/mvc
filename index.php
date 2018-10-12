1beec98b514b28a74e080963583961952628226a
Yuriy Hritsaiy <yu.hritsaiy@gmail.com>
2018-10-12 15:45:19 +0300

    routes in service container

--- index.php
+++ index.php
@@ -16,18 +16,22 @@ $container->set(JwtAuthMiddleware::class, function () {
     return new JwtAuthMiddleware();
 });
 
-$routes = new RouteCollection();
-$routes->add('home', '/', UserIndexController::class, ['GET']);
-$routes->add('view', '/view/{id}', 'App\Controllers\User\UserViewController@view', ['GET'], ['id' => '\S+']);
-$routes->add('login', '/login', AuthLoginController::class, ['POST']);
-$routes->add('secured-home', '/secured/home', [
-    $container->get(JwtAuthMiddleware::class),
-    UserIndexController::class
-], ['GET']);
+$container->set('router', function () use ($container) {
+    $routes = new RouteCollection();
+    $routes->add('home', '/', UserIndexController::class, ['GET']);
+    $routes->add('view', '/view/{id}', 'App\Controllers\User\UserViewController@view', ['GET'], ['id' => '\S+']);
+    $routes->add('login', '/login', AuthLoginController::class, ['POST']);
+    $routes->add('secured-home', '/secured/home', [
+        $container->get(JwtAuthMiddleware::class),
+        UserIndexController::class
+    ], ['GET']);
+
+    return $routes;
+});
 
 (new Application($container))
     ->pipe(\App\Middleware\ProfilerMiddleware::class)
-    ->setRoutes($routes)
+    ->setRoutes($container->get('router'))
     ->run();
 
 function dump($data)
